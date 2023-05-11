bf66e813841de7d1b85f626d0d1922ed791a3025
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-05-11 17:02:17 +0100

    run all scenarios and plots comparison of original williams and new FDA ce planes

--- scripts/Markov-model-analysis.R
+++ scripts/Markov-model-analysis.R
@@ -16,15 +16,28 @@ n_cohort <- 1000
 n_cycles <- 20
 Initial_age <- 50
 
-n_trials <- 400
+n_trials <- 1
+# n_trials <- 400
 
 # effect <- 0.1
+
+# mortality point values
+data_filename <- "fda_tpDn_wide_2041.RData"
+# data_filename <- "fda_tpDn_lower_2041.RData"
+# data_filename <- "fda_tpDn_upper_2041.RData"
 
 all_pars <- read.csv(file = "data/dsa_inputs.csv")
 
 costs <- list()
 qalys <- list()
 ce_res <- list()
+c_incr <- list()
+q_incr <- list()
+ce_orig <- list()
+costs_orig <- list()
+qalys_orig <- list()
+c_incr_orig <- list()
+q_incr_orig <- list()
 
 # deterministic scenario analysis
 
@@ -50,7 +63,8 @@ for (j in 1:nrow(all_pars)) {
   # uAsymp <- function() rbeta(1, 69, 4)
   # uProg  <- function() rbeta(1, 24, 8)
   
-  effect <- function() rnorm(1, 0.5, 0.051)
+  # effect <- function() rnorm(1, 0.5, 0.051)
+  effect <- \() 0.5
   
   # Define cost and QALYs as functions
   
@@ -83,56 +97,79 @@ for (j in 1:nrow(all_pars)) {
   }
   
   costs[[j]] <- matrix(NA, nrow = n_trials, ncol = n_treatments,
-                  dimnames = list(NULL, t_names))
+                       dimnames = list(NULL, t_names))
   qalys[[j]] <- matrix(NA, nrow = n_trials, ncol = n_treatments,
-                  dimnames = list(NULL, t_names))
+                       dimnames = list(NULL, t_names))
   
-  # mortality point values
-  data_filename <- "fda_tpDn_wide_2041.RData"
-  # data_filename <- "fda_tpDn_lower_2041.RData"
-  # data_filename <- "fda_tpDn_upper_2041.RData"
+  costs_orig[[j]] <- matrix(NA, nrow = n_trials, ncol = n_treatments,
+                            dimnames = list(NULL, t_names))
+  qalys_orig[[j]] <- matrix(NA, nrow = n_trials, ncol = n_treatments,
+                            dimnames = list(NULL, t_names))
   
   for (i in 1:n_trials) {
     print(glue::glue("trial number: {i}"))
-    ce_res[[j]] <- ce_markov(start_pop = c(n_cohort, 0, 0),
-                        n_treat = 2,
-                        n_cycles = n_cycles, 
-                        init_age = Initial_age,
-                        lambda = 0,
-                        # p_mortality_wide(filename = data_filename,                    # mortality point values
-                        #                  baseyear = 2041),
-                        # p_mortality_long(),
-                        # p_mortality_williams,    # 2016
-                        p_mortality_wide(filename = "fda_tpDn_wide_2041.RData",         # mortality prob uncertainty
-                                         filename_upper = "fda_tpDn_upper_2041.RData",
-                                         filename_lower = "fda_tpDn_lower_2041.RData"),
-                        state_c_matrix(),
-                        trans_c_matrix(),
-                        state_q_matrix())
     
-    costs[[j]][i, ] <- ce_res$total_costs
-    qalys[[j]][i, ] <- ce_res$total_QALYs
+    ce_res[[j]] <- ce_markov(start_pop = c(n_cohort, 0, 0),
+                             n_treat = 2,
+                             n_cycles = n_cycles, 
+                             init_age = Initial_age,
+                             lambda = 0,
+                             # p_mortality_wide(filename = data_filename,                    # mortality point values
+                             #                  baseyear = 2041),
+                             # p_mortality_long(),
+                             # p_mortality_williams,    # 2016
+                             p_mortality_wide(filename = "fda_tpDn_wide_2041.RData",         # mortality prob uncertainty
+                                              filename_upper = "fda_tpDn_upper_2041.RData",
+                                              filename_lower = "fda_tpDn_lower_2041.RData"),
+                             state_c_matrix(),
+                             trans_c_matrix(),
+                             state_q_matrix())
+    
+    ce_orig[[j]] <- ce_markov(start_pop = c(n_cohort, 0, 0),
+                              n_treat = 2,
+                              n_cycles = n_cycles, 
+                              init_age = Initial_age,
+                              lambda = 0,
+                              p_mortality_williams,    # 2016
+                              state_c_matrix(),
+                              trans_c_matrix(),
+                              state_q_matrix())
+    
+    costs[[j]][i, ] <- ce_res[[j]]$total_costs
+    qalys[[j]][i, ] <- ce_res[[j]]$total_QALYs
+    
+    costs_orig[[j]][i, ] <- ce_orig[[j]]$total_costs
+    qalys_orig[[j]][i, ] <- ce_orig[[j]]$total_QALYs
   }
+  
+  c_incr[[j]] <- costs[[j]][, "with_drug"] - costs[[j]][, "without_drug"]
+  q_incr[[j]] <- qalys[[j]][, "with_drug"] - qalys[[j]][, "without_drug"]
+  
+  c_incr_orig[[j]] <- costs_orig[[j]][, "with_drug"] - costs_orig[[j]][, "without_drug"]
+  q_incr_orig[[j]] <- qalys_orig[[j]][, "with_drug"] - qalys_orig[[j]][, "without_drug"]
 }
 
 ########
 # plots
 
-c_incr <- costs[, "with_drug"] - costs[, "without_drug"]
-q_incr <- qalys[, "with_drug"] - qalys[, "without_drug"]
 
 wtp <- 30000
 
-plot(x = q_incr/n_cohort, y = c_incr/n_cohort, col = "blue",
-     xlim = c(-1, 2),
-     # xlim = c(-0.5, 1),  # 80 y/o
-     ylim = c(0, 15e3),
-     # ylim = c(0, 10e3),  # 80 y/o
-     pch = 16, cex = 1.2,
-     xlab = "QALY difference",
-     ylab = paste0("Cost difference (", enc2utf8("\u00A3"), ")"),
-     frame.plot = FALSE)
-points(x = mean(q_incr, na.rm = TRUE)/n_cohort, y = mean(c_incr, na.rm = TRUE)/n_cohort,
-       col = "red", pch = 16, cex = 1.5)
-abline(a = 0, b = wtp, lwd = 2)  # willingness-to-pay threshold
+par(mfrow=c(3,3))
+for (j in seq_along(c_incr)) {
+  plot(x = q_incr[[j]]/n_cohort, y = c_incr[[j]]/n_cohort, col = "blue",
+       xlim = c(-1, 2),
+       # # xlim = c(-0.5, 1),  # 80 y/o
+       ylim = c(-2000, 15e3),
+       # # ylim = c(0, 10e3),  # 80 y/o
+       pch = 16, cex = 1.2,
+       xlab = "QALY difference",
+       ylab = paste0("Cost difference (", enc2utf8("\u00A3"), ")"),
+       frame.plot = FALSE)
+  points(x = mean(q_incr[[j]], na.rm = TRUE)/n_cohort, y = mean(c_incr[[j]], na.rm = TRUE)/n_cohort,
+         col = "red", pch = 16, cex = 1.5)
+  points(x = mean(q_incr_orig[[j]], na.rm = TRUE)/n_cohort, y = mean(c_incr_orig[[j]], na.rm = TRUE)/n_cohort,
+         col = "green", pch = 16, cex = 1.5)
+  abline(a = 0, b = wtp, lwd = 2)  # willingness-to-pay threshold
+}
 
